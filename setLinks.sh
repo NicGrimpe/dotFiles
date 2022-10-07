1b5a12fdd66a80cb4deec841a9aecc73f8d53b8f
nicG <ngariepy@ideoconcepts.ca>
2022-10-06 21:32:50 -0400

    change background

--- setLinks.sh
+++ setLinks.sh
@@ -4,7 +4,7 @@ ln -nfs $HOME/dotFiles/.xinitrc $HOME/.xinitrc
 ln -nfs $HOME/dotFiles/.bashrc $HOME/.bashrc
 
 ln -nfs $HOME/dotFiles/.fehbg $HOME/.fehbg
-ln -nfs $HOME/dotFiles/assets/mountain.png $HOME/.wallpaper
+ln -nfs $HOME/dotFiles/assets/squigglyLines.jpeg $HOME/.wallpaper
 
 ln -nfs $HOME/dotFiles/config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 ln -nfs $HOME/dotFiles/config/i3/config $HOME/.config/i3/config
